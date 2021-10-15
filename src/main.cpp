48b1a5aa4098c4d0da3b54975659d5e75bd010f9
jaume <jaume@westial.com>
2021-10-15 10:32:59 +0900

    commotalkino-1: EByte integration * In progress CommoTalkie update

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,7 @@
 #include "main.h"
 
-#include "../lib/CommoTalkie/Publish.h"
-
-SoftwareSerial SSerial(PIN_RX, PIN_TX);
-
-Driver lora_driver;
-
-unsigned long Write_To_Serial(void *content, unsigned long size) {
-    return SSerial.write((char*)content, size);
-}
-
-Driver Create_Driver(const char *topic, const char air_data_rate,
-                     const int is_fixed, const int full_power) {
-  PinMap pins = {PIN_M0, PIN_M1, PIN_AX};
-  RadioParams params = {{topic[DRIVER_ADDRESS_HIGH_INDEX],      topic[DRIVER_ADDRESS_LOW_INDEX]}, topic[2],
-                        air_data_rate, is_fixed, full_power};
-  Timer timer = Timer_Create((const void *)millis);
-  IOCallback io = {digitalRead, digitalWrite, Write_To_Serial};
-  return Driver_Create(pins, &params, &io, timer, 5000);
-}
-
-void Stub_Publish() {
-  const char address[3] = {ADDRESS_HIGH, ADDRESS_LOW, CHANNEL};
-  Publish_Invoke(address, 0x05, 0x06, "23456789A");
-}
-
-unsigned long Transmit(
-    const char* address,
-    const char* content,
-    const unsigned long size) {
-  char destination[2];
-  destination[DRIVER_ADDRESS_HIGH_INDEX] = address[0];
-  destination[DRIVER_ADDRESS_LOW_INDEX] = address[1];
-  char channel = address[2];
-  return Driver_Send(&lora_driver, destination, &channel, content, size);
-}
-
-int InitPublisher() {
-  PublisherBuilder_Create();
-  PublisherBuilder_SetSalt(COMMOTALKIE_SALT);
-  PublisherBuilder_SetSendCallback(Transmit);
-  const int result = PublisherBuilder_Build();
-  if (!result) {
-    Serial.println("Error: Publisher builder");
-  }
-  PublisherBuilder_Destroy();
-  return result;
-}
-
 void setup() {
-  Serial.begin(SERIAL_FREQ);
-  SSerial.begin(9600);
-  Serial.println("Initializing EByte Driver");
-  //lora_driver = Create_Driver("\x08\x02\x10", AIR_RATE_2400, 1, 1);
-  Serial.println("Initializing Publisher");
-  //InitPublisher();
 }
 
 void loop() {
-  const char address[3] = {ADDRESS_HIGH, ADDRESS_LOW, CHANNEL};
-  const char body[] = "012345678";
-  //Transmit(address, body, 10);
-  //Stub_Publish();
-  // let the use know something was sent
-  Serial.print("Sending: "); Serial.println(millis());
-  delay(10000);
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,13 +1,13 @@
-#include <Arduino.h>
-#include <unity.h>
 #include "test_main.h"
 #include "../lib/CommoTalkie/Message.h"
-#include "../lib/CommoTalkie/MessagePublisher.h"
 #include "../lib/CommoTalkie/MessageFormatter.h"
+#include "../lib/CommoTalkie/MessagePublisher.h"
 #include "../lib/CommoTalkie/Publish.h"
+#include <Arduino.h>
+#include <unity.h>
 
 static char spy_pushed_content[MESSAGE_LENGTH];
-static void print_chars(const char*, unsigned long);
+static void print_chars(const char *, unsigned long);
 
 // -----------------------------------------------------------------------------
 
@@ -16,12 +16,10 @@ Driver lora_driver;
 
 extern "C" unsigned long SpyWriteToSerial(void *content, unsigned long size) {
   memcpy(spy_pushed_content, (char *)content + 3, MESSAGE_LENGTH);
-  return SSerial.write((char*)content, size);
+  return SSerial.write((char *)content, size);
 }
 
-extern "C" int DigitalRead(unsigned char pin) {
-  return digitalRead(pin);
-}
+extern "C" int DigitalRead(unsigned char pin) { return digitalRead(pin); }
 
 extern "C" void DigitalWrite(unsigned char pin, unsigned char value) {
   digitalWrite(pin, value);
@@ -51,22 +49,21 @@ int InitPublisher() {
 Driver Create_Driver(const char *topic, const char air_data_rate,
                      const int is_fixed, const int full_power) {
   PinMap pins = {PIN_M0, PIN_M1, PIN_AX};
-  RadioParams params = {{topic[DRIVER_ADDRESS_HIGH_INDEX],      topic[DRIVER_ADDRESS_LOW_INDEX]}, topic[2],
-                        air_data_rate, is_fixed, full_power};
+  RadioParams params = {
+      {topic[DRIVER_ADDRESS_HIGH_INDEX], topic[DRIVER_ADDRESS_LOW_INDEX]},
+      topic[2],
+      air_data_rate,
+      is_fixed,
+      full_power};
   Timer timer = Timer_Create((const void *)Millis);
   IOCallback io = {DigitalRead, DigitalWrite, SpyWriteToSerial};
   return Driver_Create(pins, &params, &io, timer, 5000);
 }
 
-unsigned long Transmit(
-    const char* address,
-    const char* content,
-    const unsigned long size) {
-  char destination[2];
-  destination[DRIVER_ADDRESS_HIGH_INDEX] = address[0];
-  destination[DRIVER_ADDRESS_LOW_INDEX] = address[1];
-  char channel = address[2];
-  return Driver_Send(&lora_driver, destination, &channel, content, size);
+unsigned long Transmit(const char *address, const char *content,
+                       const unsigned long size) {
+  const Destination target = {address[0], address[1], address[2]};
+  return Driver_Send(&lora_driver, &target, content, size);
 }
 
 void setUp(void) {
@@ -78,22 +75,19 @@ void setUp(void) {
   InitPublisher();
 }
 
-void tearDown(void) {
-  Publish_Destroy();
-}
+void tearDown(void) { Publish_Destroy(); }
 
 void test_publish() {
   const char body[] = "23456789A";
   const char address[3] = {ADDRESS_HIGH, ADDRESS_LOW, CHANNEL};
   Publish_Invoke(address, 0x05, 0x06, body);
-  TEST_ASSERT_EQUAL_CHAR_ARRAY(body + 1,
-                               spy_pushed_content + 4,
+  TEST_ASSERT_EQUAL_CHAR_ARRAY(body + 1, spy_pushed_content + 4,
                                MESSAGE_BODY_LENGTH - 1);
 }
 
 // -----------------------------------------------------------------------------
 
-void print_chars(const char* anArray, unsigned long size) {
+void print_chars(const char *anArray, unsigned long size) {
   char printable[size + 1];
   memcpy(printable, anArray, size);
   printable[size] = '\0';
@@ -110,7 +104,7 @@ void setup() {
 }
 
 void loop() {
-  //RUN_TEST(test_push_message);
+  // RUN_TEST(test_push_message);
   RUN_TEST(test_publish);
   UNITY_END();
   delay(5000);
